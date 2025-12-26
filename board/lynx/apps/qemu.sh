77a0f1d8cd4cf4f74963814d48d96e96ca46123c
Ray Jk Hong <ray.jk.hong@gmail.com>
2025-12-26 14:18:46 +0800

    Use rootfs.cpio.gz as kvm initrd

--- board/lynx/apps/qemu.sh
+++ board/lynx/apps/qemu.sh
@@ -1,1 +1,1 @@
-./apps/lkvm-static run -m 128 -c2 --console serial -p "console=ttyS0 earlycon=sbi" -k ./apps/Image -i ./apps/initramfs.cpio.gz --debug
+./apps/lkvm-static run -m 128 -c2 --console serial -p "console=ttyS0 earlycon=sbi" -k ./apps/Image -i ./apps/rootfs.cpio.gz --debug
